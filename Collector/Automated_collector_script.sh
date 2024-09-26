e95c2c67010454bc1aa42a2cd7a8ca3691c2f790
Johnolofs <70148035+johnolofs@users.noreply.github.com>
2024-09-26 16:38:03 +0200

    Automated_collector_script.sh

--- Collector/Automated_collector_script.sh
+++ Collector/Automated_collector_script.sh
@@ -58,20 +58,24 @@ sudo apt install -y unrar || handle_error
 
 # Unpack The RAR Files To Timpi Directory For Upgrade
 echo "Upgrading to the collector version 0.9.3..."
-sudo unrar x "$INSTALL_DIR/TimpiCollectorLinuxLatest.rar" "$INSTALL_DIR" || handle_error
+sudo unrar x "$INSTALL_DIR/TimpiCollectorLinuxLatest.rar" -d "$INSTALL_DIR" || handle_error
 
-# Move the TimpiCollector and TimpiUI files to the correct directory
+# Move the extracted files (TimpiCollector, TimpiUI) to the correct directory
 echo "Moving TimpiCollector and TimpiUI to $INSTALL_DIR..."
-sudo mv "$INSTALL_DIR/TimpiCollectorLinuxLatest/TimpiCollector" "$INSTALL_DIR/" || handle_error
-sudo mv "$INSTALL_DIR/TimpiCollectorLinuxLatest/TimpiUI" "$INSTALL_DIR/" || handle_error
+sudo mv "$INSTALL_DIR/TimpiCollector-0-9-3-Linux-update/TimpiCollector" "$INSTALL_DIR/" || handle_error
+sudo mv "$INSTALL_DIR/TimpiCollector-0-9-3-Linux-update/TimpiUI" "$INSTALL_DIR/" || handle_error
+sudo mv "$INSTALL_DIR/TimpiCollector-0-9-3-Linux-update/0.9.3.txt" "$INSTALL_DIR/" || handle_error
 
 # Setting correct permissions for TimpiCollector and TimpiUI
 echo "Setting execute permissions for TimpiCollector and TimpiUI..."
 sudo chmod 755 "$INSTALL_DIR/TimpiCollector" || handle_error
 sudo chmod 755 "$INSTALL_DIR/TimpiUI" || handle_error
 
-# Clean up by removing the temporary unpacked folder
-sudo rm -rf "$INSTALL_DIR/TimpiCollectorLinuxLatest" || handle_error
+# Clean up by removing the temporary unpacked folder and old archives
+echo "Cleaning up..."
+sudo rm -rf "$INSTALL_DIR/TimpiCollector-0-9-3-Linux-update" || handle_error
+sudo rm -rf "$INSTALL_DIR/TimpiCollectorLinuxLatest.rar" || handle_error
+sudo rm -rf "$INSTALL_DIR/TimpiCollector-0-9-0-Linux.zip" || handle_error
 
 # Enable and start services
 sudo systemctl enable collector || handle_error

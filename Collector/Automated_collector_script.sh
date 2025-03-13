fdc379a916ed9d6d1d0795b2622fbf6a29bf80f8
Johnolofs <70148035+johnolofs@users.noreply.github.com>
2025-03-13 19:17:26 +0100

    Update Automated_collector_script.sh

--- Collector/Automated_collector_script.sh
+++ Collector/Automated_collector_script.sh
@@ -22,42 +22,20 @@ sudo apt update && sudo apt -y upgrade || handle_error
 # Create directory for Timpi
 sudo mkdir -p "$INSTALL_DIR" || handle_error
 
-# Download the collector 0.9.0
-echo "Downloading the collector 0.9.0..."
-sudo wget https://timpi.io/applications/linux/TimpiCollector-0-9-0-Linux.zip -O "$INSTALL_DIR/TimpiCollector-0-9-0-Linux.zip" || handle_error
-
 # Install Unzip if not already installed
 echo "Installing unzip..."
 sudo apt install -y unzip || handle_error
-
-# Unpack The Zip Files
-echo "Unpacking the collector 0.9.0..."
-sudo unzip "$INSTALL_DIR/TimpiCollector-0-9-0-Linux.zip" -d "$INSTALL_DIR" || handle_error
-
-# Install the Collector 0.9.0
-echo "Installing the collector 0.9.0..."
-cd "$INSTALL_DIR" || handle_error
-sudo unzip TimpiCollector-0-9-0-beta.zip -d "$INSTALL_DIR" || handle_error
-
-# Setting execute rights
-sudo chmod +x "$INSTALL_DIR/TimpiCollector" || handle_error
-sudo chmod +x "$INSTALL_DIR/TimpiUI" || handle_error
-
-# Install Collector service
-echo "Installing Collector service..."
-sudo mv "$INSTALL_DIR/collector.service" /etc/systemd/system/ || handle_error
-sudo mv "$INSTALL_DIR/collector_ui.service" /etc/systemd/system/ || handle_error
-
-# Download the latest collector version (0.9.4)
-echo "Downloading the latest collector version..."
-sudo wget https://timpi.io/applications/linux/TimpiCollectorLinuxLatest.rar -O "$INSTALL_DIR/TimpiCollectorLinuxLatest.rar" || handle_error
 
 # Install Unrar if not already installed
 echo "Installing unrar..."
 sudo apt install -y unrar || handle_error
 
+# Download the latest collector version (0.9.5)
+echo "Downloading the latest collector version (0.9.5)..."
+sudo wget https://timpi.io/applications/linux/TimpiCollectorLinuxLatest.rar -O "$INSTALL_DIR/TimpiCollectorLinuxLatest.rar" || handle_error
+
 # Unpack The RAR Files To Timpi Directory For Upgrade, overwriting any existing files
-echo "Upgrading to the latest collector version..."
+echo "Upgrading to the latest collector version (0.9.5)..."
 sudo unrar e -y "$INSTALL_DIR/TimpiCollectorLinuxLatest.rar" "$INSTALL_DIR" || handle_error
 
 # Setting correct permissions for TimpiCollector and TimpiUI
@@ -65,10 +43,14 @@ echo "Setting execute permissions for TimpiCollector and TimpiUI..."
 sudo chmod 755 "$INSTALL_DIR/TimpiCollector" || handle_error
 sudo chmod 755 "$INSTALL_DIR/TimpiUI" || handle_error
 
+# Install Collector service
+echo "Installing Collector service..."
+sudo mv "$INSTALL_DIR/collector.service" /etc/systemd/system/ || handle_error
+sudo mv "$INSTALL_DIR/collector_ui.service" /etc/systemd/system/ || handle_error
+
 # Clean up by removing the old archives
 echo "Cleaning up..."
 sudo rm -rf "$INSTALL_DIR/TimpiCollectorLinuxLatest.rar" || handle_error
-sudo rm -rf "$INSTALL_DIR/TimpiCollector-0-9-0-Linux.zip" || handle_error
 
 # Enable and start services
 sudo systemctl enable collector || handle_error

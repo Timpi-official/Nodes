7886f876409aa646385764de0c6a72bda6120558
Johnolofs <70148035+johnolofs@users.noreply.github.com>
2025-04-24 21:51:21 +0200

    Update TimpiGuardianLatest.sh

--- Guardian/TimpiGuardianLatest.sh
+++ Guardian/TimpiGuardianLatest.sh
@@ -29,7 +29,7 @@ echo -e "\n✅ Location set to: $LOCATION"
 # Run the Docker container
 echo -e "\n🚀 Starting Timpi Guardian container..."
 sudo docker run -d --pull=always --restart unless-stopped \
-  --dns=100.42.180.116 --dns=8.8.8.8 \
+  --dns=100.42.180.116 --dns=212.28.186.105 --dns=8.8.8.8 \
   -p ${SOLR_PORT}:${SOLR_PORT} \
   -p ${GUARDIAN_PORT}:${GUARDIAN_PORT} \
   -v /var/solrdocker:/var/solr \

e0a0c3f1f44d18c0527d6d78d57f39628a498669
Johnolofs <70148035+johnolofs@users.noreply.github.com>
2026-05-09 09:16:51 +0200

    Update install.sh

--- Synaptron/install.sh
+++ Synaptron/install.sh
@@ -13,7 +13,7 @@ set -euo pipefail
 # - Runs run_synaptron.sh
 ###########################################################
 
-REPO_BASE="https://raw.githubusercontent.com/johnolofs/timpi/main/Synaptron/scripts"
+REPO_BASE="https://raw.githubusercontent.com/Timpi-official/Nodes/main/Synaptron"
 INSTALL_DIR="${HOME}/Synaptron"
 YML_FILE="docker-compose.yml"
 RUN_FILE="run_synaptron.sh"
